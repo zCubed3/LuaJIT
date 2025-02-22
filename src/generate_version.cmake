e0491ac6be852ac61e384148ce125bc545488cab
Liam Reese <zcubedtres@gmail.com>
2025-02-21 22:18:39 -0800

    Version generation now works without git (windows only atm)

--- src/generate_version.cmake
+++ src/generate_version.cmake
@@ -8,8 +8,19 @@ if (EXISTS ${SOURCE_DIR}/../.git)
 
     if (NOT GIT_RESULT EQUAL 0)
         message(WARNING "Git show failed, using .relver")
-        file(COPY ${SOURCE_DIR}/../.relver DESTINATION ${SOURCE_DIR}/luajit_relver.txt)
+
+        # TODO: This is windows only, fix this elsewhere
+        execute_process(
+                COMMAND type ${SOURCE_DIR}/../.relver
+                WORKING_DIRECTORY ${SOURCE_DIR}
+                OUTPUT_FILE ${SOURCE_DIR}/luajit_relver.txt
+        )
     endif()
 else()
-    file(COPY ${SOURCE_DIR}/../.relver DESTINATION ${SOURCE_DIR}/luajit_relver.txt)
+    # TODO: This is windows only, fix this elsewhere
+    execute_process(
+            COMMAND type ${SOURCE_DIR}/../.relver
+            WORKING_DIRECTORY ${SOURCE_DIR}
+            OUTPUT_FILE ${SOURCE_DIR}/luajit_relver.txt
+    )
 endif()
